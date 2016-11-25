1a93df62216339bb1211fa7fe004252af96b3749
Anton Kondrashov <kondrashka1313@yandex.ru>
2016-11-25 03:39:41 +0300

    Initial Commit

---
+++ BottleGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BottleGame
+//
+//  Created by Anton Kondrashov on 25/11/2016.
+//  Copyright © 2016 Anton Kondrashov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
